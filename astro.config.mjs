672df694805a2ba26f89a5932e82b2bb199b8978
joshua-poon <86510885+joshua-poon@users.noreply.github.com>
2025-06-12 16:10:39 +1200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	output: 'static',
+	base: '/poonos-astro/'
+
 });
