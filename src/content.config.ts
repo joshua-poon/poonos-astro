5a718c989757f270c123cf2276d9d88fb50fd154
Joshua Poon <joshua.poon21@gmail.com>
2025-06-22 16:17:22 +1000

    Latest Post fix and NZ

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,35 +1,6 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const linguistics = defineCollection({
-	// Load Markdown and MDX files in the `src/content/linguistics/` directory.
-	loader: glob({ base: './src/content/linguistics', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
-});
-
-const medicine = defineCollection({
-	// Load Markdown and MDX files in the `src/content/medicine/` directory.
-	loader: glob({ base: './src/content/medicine', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
-});
-
-
 const blog = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
@@ -45,4 +16,4 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { linguistics, medicine, blog, };
+export const collections = { blog, };
